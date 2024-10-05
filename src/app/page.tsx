30dc2c6da5672389e060527fcd9dc0a96a850cff
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-10-05 19:51:42 +0300

    search form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import SearchForm from "@/components/search-form";
 import Link from "next/link";
+import { FormEvent } from "react";
 
 export default function Home() {
   return (
@@ -8,19 +10,11 @@ export default function Home() {
       </h1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
-        <span className="font-bold text-accent italic underline">
-          10,000
-        </span>{" "}
+        <span className="font-bold text-accent italic underline">10,000</span>{" "}
         events around you
       </p>
 
-      <form className="w-full sm:w-[580px]">
-        <input
-          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
-          placeholder="Search events in any city..."
-          spellCheck={false}
-        />
-      </form>
+      <SearchForm />
 
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular:</p>
@@ -32,3 +26,4 @@ export default function Home() {
     </main>
   );
 }
+ 
