842867dcec3bf06d7fb1804054e7fa7b8e0548b9
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-10-09 21:27:58 +0300

    added cn() utility func

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,5 +1,5 @@
+import { cn } from "@/lib/utils";
 import React from "react";
-import { twMerge } from "tailwind-merge";
 
 type H1Props = {
   children: React.ReactNode;
@@ -7,7 +7,7 @@ type H1Props = {
 };
 export default function H1({ children, className }: H1Props) {
   return (
-    <h1 className={twMerge("text-3xl lg:text-6xl font-bold tracking-tight", className)}>
+    <h1 className={cn("text-3xl lg:text-6xl font-bold tracking-tight", className)}>
       {children}
     </h1>
   );
