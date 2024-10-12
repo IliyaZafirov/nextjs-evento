d6053b9b02939c9496071a94ab09b0dece7f17ed
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-10-12 18:10:16 +0300

    finished loading EventList

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,6 +1,8 @@
+import Loading from "@/app/events/[city]/loading";
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
+import { Suspense } from "react";
+
 type EventsPageProps = {
   params: {
     city: string;
@@ -8,22 +10,19 @@ type EventsPageProps = {
 };
 
 export default async function EventsPage({ params }: EventsPageProps) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${params.city}`
-  );
-  const events: EventoEvent[] = await response.json();
+  const city = params.city;
 
   return (
-    <main className="flex flex-col items-center  py-24 px-[20px] min-h-[110vh]">
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
-        {params.city === "all" && "All Events"}
-        {params.city !== "all" &&
-          `Events in ${
-            params.city.charAt(0).toUpperCase() + params.city.slice(1)
-          }`}
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
