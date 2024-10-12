a630bd92062157aaa6c5817a48a31149e82d60f2
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-10-12 12:11:36 +0300

    added SkeletonCard component

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import H1 from "@/components/h1";
-import { sleep } from "@/lib/utils";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -11,7 +10,6 @@ type EventPageProps = {
 export default async function EventPage({ params }: EventPageProps) {
   const slug = params.slug;
 
-  await sleep(4000);
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -24,7 +24,6 @@ export default async function EventsPage({ params }: EventsPageProps) {
       </H1>
 
       <EventsList events={events} />
-   
     </main>
   );
 }
