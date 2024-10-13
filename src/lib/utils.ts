8df78832395ae0b3951a85268bc2890dbc786106
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-10-13 12:35:55 +0300

    server-side pagination in NextJS for EventList

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,7 +17,7 @@ export async function sleep(ms: number) {
   });
 }
 
-export async function getEvents(city: string) {
+export async function getEvents(city: string, page = 1) {
   const events = await prisma.eventoEvent.findMany({
     where: {
       city: city === "all" ? undefined : capitalize(city),
@@ -25,8 +25,21 @@ export async function getEvents(city: string) {
     orderBy: {
       date: "asc",
     },
+    take: 6,
+    skip: (page - 1) * 6,
   });
-  return events;
+
+  let totalCount;
+  if (city === "all") {
+    totalCount = await prisma.eventoEvent.count();
+  } else {
+    totalCount = await prisma.eventoEvent.count({
+      where: {
+        city: capitalize(city),
+      },
+    });
+  }
+  return { events, totalCount };
 }
 
 export async function getEvent(slug: string) {
@@ -39,6 +52,6 @@ export async function getEvent(slug: string) {
   if (!event) {
     return notFound();
   }
-   
+
   return event;
 }
